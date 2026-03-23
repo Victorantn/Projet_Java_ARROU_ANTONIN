9ab47ea5b19647f756bba603458409f27b6c74cd
Victorantn <victor.antonin31@gmail.com>
2026-03-23 13:10:49 +0100

    Suppression MDP et URL en clair

--- AppliUniv/src/main/java/org/example/ConnexionBD.java
+++ AppliUniv/src/main/java/org/example/ConnexionBD.java
@@ -1,14 +1,32 @@
 package org.example;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnexionBD {
-    private static final String url = "jdbc:oracle:thin:@//telline.univ-tlse3.fr:1521/etupre";
-    private static final String usr = "ntv4762a";
-    private static final String pdm = "PaulSabatier31$O";
-
+    private static final String url;
+    private static final String usr;
+    private static final String pdm;
     private static Connection con;
+
+    static {
+        Properties props = new Properties();
+        try (InputStream is = ConnexionBD.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (is == null) {
+                throw new RuntimeException("Fichier db.properties introuvable dans les ressources");
+            }
+            props.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException("Erreur lecture db.properties", e);
+        }
+        url = props.getProperty("db.url");
+        usr = props.getProperty("db.user");
+        pdm = props.getProperty("db.password");
+    }
 
     /**
      * <p>Fonction qui nous sert à récupérer la connexion unique à la BD</p>
